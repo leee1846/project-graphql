fdda42fd0212982b3cfa1c5fd9e3cf0a2ac1a956
leee1846 <leee1846@gmail.com>
2021-05-06 11:07:29 +0900

    feature: teams페이지 aside적용

--- front/src/components/teams.js
+++ front/src/components/teams.js
@@ -1,25 +1,78 @@
-import './components.css';
+import "./components.css";
+import { useState } from "react";
+import { gql, useQuery, useMutation } from "@apollo/client";
+
+const GET_TEAMS = gql`
+  query GetTeams {
+    teams {
+      id
+      manager
+      members {
+        id
+        first_name
+        last_name
+        role
+      }
+    }
+  }
+`;
 
 function Teams() {
+  const [contentId, setContentId] = useState(0);
+  const [inputs, setInputs] = useState({
+    manager: "",
+    offices: "",
+    extension_number: "",
+    mascot: "",
+    cleaning_duty: "",
+    project: "",
+  });
 
-    function AsideItems () {
-        return (<div></div>);
-    }
+  function AsideItems() {
+    const roleIcons = {
+      developer: "💻",
+      designer: "🎨",
+      planner: "📝",
+    };
 
-    function MainContents () {
-        return (<div></div>);
-    }
+    const { loading, error, data, refetch } = useQuery(GET_TEAMS);
 
+    if (loading) return <p className='loading'>Loading...</p>;
+    if (error) return <p className='error'>Error...</p>;
     return (
-        <div id="teams" className="component">
-            <aside>
-                {AsideItems()}
-            </aside>
-            <section className="contents">
-                {MainContents()}
-            </section>
-        </div>
-    )
+      <ul>
+        {data.teams.map(({ id, manager, members }) => {
+          return (
+            <li key={id}>
+              <span className='teamItemTitle' onClick={() => setContentId(id)}>
+                Team {id} : {manager}'s
+              </span>
+              <ul className='teamMembers'>
+                {members.map(({ id, first_name, last_name, role }) => {
+                  return (
+                    <li key={id}>
+                      {roleIcons[role]} {first_name} {last_name}
+                    </li>
+                  );
+                })}
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+
+  function MainContents() {
+    return <div></div>;
+  }
+
+  return (
+    <div id='teams' className='component'>
+      <aside>{AsideItems()}</aside>
+      <section className='contents'>{MainContents()}</section>
+    </div>
+  );
 }
 
 export default Teams;
