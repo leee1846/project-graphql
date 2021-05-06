a24e1377d20b12d40b9e4a84b3c2e460b7046954
leee1846 <leee1846@gmail.com>
2021-05-06 09:46:29 +0900

    feature:사이드바 적용

--- front/src/components/roles.js
+++ front/src/components/roles.js
@@ -1,25 +1,58 @@
-import './components.css';
+import "./components.css";
+import { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_ROLES = gql`
+  query GetRoles {
+    roles {
+      id
+    }
+  }
+`;
 
 function Roles() {
+  const [contentId, setContentId] = useState("");
 
-    function AsideItems () {
-        return (<div></div>);
-    }
-
-    function MainContents () {
-        return (<div></div>);
-    }
+  function AsideItems() {
+    const roleIcons = {
+      developer: "💻",
+      designer: "🎨",
+      planner: "📝",
+    };
+    const { loading, error, data } = useQuery(GET_ROLES);
+    if (loading) return <p className='loading'>Loading...</p>;
+    if (error) return <p className='error'>Error :</p>;
 
     return (
-        <div id="roles" className="component">
-            <aside>
-                {AsideItems()}
-            </aside>
-            <section className="contents">
-                {MainContents()}
-            </section>
-        </div>
-    )
+      <ul>
+        {data.roles.map(({ id }) => {
+          return (
+            <li
+              key={id}
+              className={"roleItem " + (contentId === "id" ? "on" : "")}
+              onClick={() => {
+                setContentId(id);
+              }}
+            >
+              <span>{contentId === id ? "🔲" : "⬛"}</span>
+              {roleIcons[id]} {id}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+
+  function MainContents() {
+    return <div></div>;
+  }
+
+  return (
+    <div id='roles' className='component'>
+      <aside>{AsideItems()}</aside>
+      <section className='contents'>{MainContents()}</section>
+    </div>
+  );
 }
 
 export default Roles;
